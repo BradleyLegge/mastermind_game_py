34500d62625c4515702ce85989b2659a7a1d1c02
BradleyLegge <bmlegge12@gmail.com>
2025-07-26 17:18:53 -0400

    added final functionality

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ def main():
     guess = int(input("Guess the 4 digit number: "))
     print("")
 
-    mastermind_num_str = str(mastermind_num)
-    guess_str = str(guess)
+    attempts = 1
 
-    max_len = max(len(mastermind_num_str), len(guess_str))
-    mastermind_num_str = mastermind_num_str.zfill(max_len)
-    guess_str = guess_str.zfill(max_len)
-
-    while(mastermind_num != guess_str):
-        attempts = 0
+    while(mastermind_num != guess):
+        mastermind_num_str = str(mastermind_num)
+        guess_str = str(guess)
         count = 0
         answer = []
+        
+        max_len = max(len(mastermind_num_str), len(guess_str))
+        mastermind_num_str = mastermind_num_str.zfill(max_len)
+        guess_str = guess_str.zfill(max_len)
+
         for i, (d1, d2) in enumerate(zip(mastermind_num_str, guess_str)):
             if d1 == d2:
-                print(mastermind_num_str)
-                print(guess_str)
                 answer.append(d1)
                 count += 1
             else:
                 answer.append("X")
 
         attempts += 1
+        print(answer)
         correct = " ".join(answer)
         print(f"Not quite the number. But you did grt {count} digit(s) correct!")
         print("Also these numbers in your input were correct.")
@@ -35,7 +35,8 @@ def main():
 
         guess = int(input("Enter your next choice of numbers: "))
 
-
+    print("You've become a Mastermind.")
+    print(f"It took you only {attempts} tries")
 
 if __name__  == "__main__":
     main()
